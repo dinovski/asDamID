9297db8d7e869f3560d79110d478e2226c5249d9
Dina <dinovski@users.noreply.github.com>
2018-09-14 22:45:29 +0300

    Update hmmt.R

--- scripts/hmmt.R
+++ scripts/hmmt.R
@@ -73,18 +73,13 @@ lapply(counts.in, function(file) {
   #countTable<-countTable[countTable$FUSION_1 + countTable$DAM_1 > min_reads & countTable$FUSION_2 + countTable$DAM_2 > min_reads,]
   countTable<-countTable[countTable$FUSION_1 + countTable$DAM_1 > min_reads,]
   
-  ## normalization
   ## replicate 1
+  ## normalization
   countTable$FUSION_NORM_1<-countTable$FUSION_1/sum(countTable$FUSION_1) * 1e6 + pseudo
   countTable$DAM_NORM_1<-countTable$DAM_1/sum(countTable$DAM_1) * 1e6 + pseudo
   countTable$logNORM_1<-log2(countTable$FUSION_NORM_1/countTable$DAM_NORM_1)
   
-  ## replicate 2
-  #countTable$FUSION_NORM_2<-countTable$FUSION_2/sum(countTable$FUSION_2) * 1e6 + pseudo
-  #countTable$DAM_NORM_2<-countTable$DAM_2/sum(countTable$DAM_2) * 1e6 + pseudo
-  #countTable$logNORM_2<-log2(countTable$FUSION_NORM_2/countTable$DAM_NORM_2)
-  
-  ## call LADs on Rep1
+  ## call LADs
   normData.1 <- countTable[,c("CHR","START","END","logNORM_1")]
   colnames(normData.1) = c("chr", "start", "end", "score")
   normData.1 = normData.1[order(normData.1$chr, normData.1$start),]
@@ -103,22 +98,26 @@ lapply(counts.in, function(file) {
   write.table(as(hmm_ranges.1, "data.frame")[, 1:3],
               file = output_ad.1, 
               quote = F, sep = "\t", row.names = F, col.names = F)
+  ## replicate 2
+  ## normalization
+  countTable$FUSION_NORM_2<-countTable$FUSION_2/sum(countTable$FUSION_2) * 1e6 + pseudo
+  countTable$DAM_NORM_2<-countTable$DAM_2/sum(countTable$DAM_2) * 1e6 + pseudo
+  countTable$logNORM_2<-log2(countTable$FUSION_NORM_2/countTable$DAM_NORM_2)
+  ## call LADs
+  normData.2 <- countTable[,c("CHR","START","END","logNORM_2")]
+  colnames(normData.2) = c("chr", "start", "end", "score")
+  normData.2 = normData.2[order(normData.2$chr, normData.2$start),]
   
-  ## call LADs on Rep2
-  #normData.2 <- countTable[,c("CHR","START","END","logNORM_2")]
-  #colnames(normData.2) = c("chr", "start", "end", "score")
-  #normData.2 = normData.2[order(normData.2$chr, normData.2$start),]
-  
-  #output_hmm.2 <- file.path(outPath.2, paste0(sampID, "_2_HMM.bed"))
-  #output_ad.2 <- file.path(outPath.2, paste0(sampID, "_2_AD.bed"))
+  output_hmm.2 <- file.path(outPath.2, paste0(sampID, "_2_HMM.bed"))
+  output_ad.2 <- file.path(outPath.2, paste0(sampID, "_2_AD.bed"))
   
   ## run HMM
-  #hmm_calls.2 <- HMM(normData.2, na_solution = "NA")
-  #hmm_ranges.2 <- getHMMRanges(as(hmm_calls.2, "GRanges"), score = "AD")
+  hmm_calls.2 <- HMM(normData.2, na_solution = "NA")
+  hmm_ranges.2 <- getHMMRanges(as(hmm_calls.2, "GRanges"), score = "AD")
   
   ## Write data tables
-  #write.table(hmm_calls.2, file = output_hmm.2, quote = F, sep = "\t", row.names = F, col.names = F)
+  write.table(hmm_calls.2, file = output_hmm.2, quote = F, sep = "\t", row.names = F, col.names = F)
   
-  #write.table(as(hmm_ranges.2, "data.frame")[, 1:3], file = output_ad.2, quote = F, sep = "\t", row.names = F, col.names = F)
+  write.table(as(hmm_ranges.2, "data.frame")[, 1:3], file = output_ad.2, quote = F, sep = "\t", row.names = F, col.names = F)
 })
 
